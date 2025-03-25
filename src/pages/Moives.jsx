3014ce9937a9355a5b7af9a449779a44088a3289
shubham kumar maurya <shubhamkumar234@lpu.in>
2025-03-25 15:20:46 +0530

    first2

--- src/pages/Moives.jsx
+++ src/pages/Moives.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-// import Product from "../components/Product";
+import Product from "../components/Product";
 import { lazy,Suspense } from "react";
-const Product=lazy(()=>import("../components/Product"));
+// const Product=lazy(()=>import("../components/Product"));
 const Moives = () => {
   return (
     <div>
-      <Suspense fallback={<div>Product is loading...</div>}>
+      {/* <Suspense fallback={<div>Product is loading...</div>}>
         <Product />
-      </Suspense>
+      </Suspense> */}
+      <Product/>
     </div>
   );
 };
